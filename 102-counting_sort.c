451cf155e4ae0ab6e9f6a5d4f32711c1cb48811c
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-04-09 15:11:25 +0000

    6.Counting sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,55 +1,98 @@
-#include "sort.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "sort.h"
 
 /**
- * counting_sort - Sorts an array of integers using Counting sort algorithm
- * @array: The array to be sorted
- * @size: Size of the array
+ * get_max - Finds the maximum value in an array.
+ * @array: The array to scan.
+ * @size: Size of the array.
+ *
+ * Return: Maximum integer in the array.
+ */
+int get_max(int *array, size_t size)
+{
+	size_t i;
+	int max = array[0];
+
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+	return (max);
+}
+
+/**
+ * fill_count_array - Counts occurrences of each number.
+ * @array: The input array.
+ * @size: Size of the array.
+ * @count: The count array.
+ */
+void fill_count_array(int *array, size_t size, int *count)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		count[array[i]]++;
+}
+
+/**
+ * accumulate_count_array - Turns count array into cumulative sum.
+ * @count: The count array.
+ * @max: Maximum number in original array.
+ */
+void accumulate_count_array(int *count, int max)
+{
+	int i;
+
+	for (i = 1; i <= max; i++)
+		count[i] += count[i - 1];
+}
+
+/**
+ * build_sorted_array - Reconstructs sorted array from counts.
+ * @array: Original array.
+ * @size: Size of the array.
+ * @count: Count array.
+ * @output: Output array.
+ */
+void build_sorted_array(int *array, size_t size, int *count, int *output)
+{
+	int i;
+
+	for (i = size - 1; i >= 0; i--)
+	{
+		output[count[array[i]] - 1] = array[i];
+		count[array[i]]--;
+	}
+}
+
+/**
+ * counting_sort - Sorts an array using counting sort algorithm.
+ * @array: The array to sort.
+ * @size: Size of the array.
  */
 void counting_sort(int *array, size_t size)
 {
-	size_t i, max;
-	int *count, *sorted;
+	int max, *count, *output;
+	size_t i;
 
 	if (!array || size < 2)
 		return;
 
-	max = array[0];
-	for (i = 1; i < size; i++)
-	{
-		if ((size_t)array[i] > max)
-			max = array[i];
-	}
-
-	count = calloc((max + 1), sizeof(int));
-	if (!count)
+	max = get_max(array, size);
+	count = calloc(max + 1, sizeof(int));
+	output = malloc(sizeof(int) * size);
+	if (!count || !output)
 		return;
 
-	for (i = 0; i < size; i++)
-		count[array[i]]++;
-
-	for (i = 1; i <= max; i++)
-		count[i] += count[i - 1];
-
-	print_array(count, max + 1);
-
-	sorted = malloc(sizeof(int) * size);
-	if (!sorted)
-	{
-		free(count);
-		return;
-	}
-
-	for (i = size; i > 0; i--)
-	{
-		sorted[count[array[i - 1]] - 1] = array[i - 1];
-		count[array[i - 1]]--;
-	}
+	fill_count_array(array, size, count);
+	accumulate_count_array(count, max);
+	build_sorted_array(array, size, count, output);
 
 	for (i = 0; i < size; i++)
-		array[i] = sorted[i];
+		array[i] = output[i];
 
-	free(sorted);
 	free(count);
+	free(output);
 }
